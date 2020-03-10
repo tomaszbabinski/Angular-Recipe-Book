f13a37ce9818c5f2f8f45f62c956b30c6ed6657a
tomaszbabinski <tommybab@gmail.com>
2020-03-10 19:36:17 +0100

    Closing dropdown with any click

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
@@ -6,7 +6,12 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    console.log(event.target);
+  }
+
+  constructor(private elRef: ElementRef) {
+
   }
 }
