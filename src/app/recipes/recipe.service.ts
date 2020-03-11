68fa6575733fc97297f363bedcd5916ec6cc52ba
tomaszbabinski <tommybab@gmail.com>
2020-03-11 16:09:22 +0100

    Passing data from recipes to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -18,7 +20,13 @@ export class RecipeService {
       new Ingredient('pepper', 1)])
   ];
 
+  constructor(private shService: ShoppingListService) { }
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shService.addIngredients(ingredients);
   }
 }
