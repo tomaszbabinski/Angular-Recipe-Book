bb066c13eff76f3f6bbb5c40d50c382c16c429f6
tomaszbabinski <tommybab@gmail.com>
2020-03-11 15:50:01 +0100

    Adding ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,21 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test recipe', 'This is simply test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Another Test recipe', 'This is simply test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('A Test recipe',
+      'This is simply test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [new Ingredient('sausage', 1),
+      new Ingredient('spinach', 2)]),
+    new Recipe('Another Test recipe',
+      'This is simply test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [new Ingredient('butter', 3),
+      new Ingredient('pepper', 1)])
   ];
 
   getRecipes() {
