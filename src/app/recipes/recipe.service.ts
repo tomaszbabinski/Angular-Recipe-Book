8c03fbb5fd1996485e5508a33b3150b7d133ce0f
tomaszbabinski <tommybab@gmail.com>
2020-04-13 13:57:21 +0200

    Fetching recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -24,6 +24,12 @@ export class RecipeService {
 
   constructor(private shService: ShoppingListService) { }
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+
+  }
+
   getRecipes() {
     return this.recipes.slice();
   }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -12,5 +13,11 @@ export class DataStorageService {
       .subscribe(response => {
         console.log(response);
       });
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://ng-course-recipe-book-fbbb5.firebaseio.com/recipes.json').subscribe(recipes => {
+      this.recipeService.setRecipes(recipes);
+    });
   }
 }
