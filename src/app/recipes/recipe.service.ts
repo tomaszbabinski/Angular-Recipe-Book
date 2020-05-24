0bb16e442c573c489ffb9bc1a88702df4417de22
tomaszbabinski <tommybab@gmail.com>
2020-05-24 21:52:58 +0200

    Dispatching action for add ingredients to shopping list from Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Subject } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
 
 @Injectable()
 export class RecipeService {
@@ -24,7 +26,7 @@ export class RecipeService {
 
   private recipes: Recipe[] = [];
 
-  constructor(private shService: ShoppingListService) { }
+  constructor(private shService: ShoppingListService, private store: Store<{ ingredients: { ingredients: Ingredient[] } }>) { }
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
@@ -41,7 +43,8 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.shService.addIngredients(ingredients);
+    // this.shService.addIngredients(ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
   }
 
   addRecipe(recipe: Recipe) {
