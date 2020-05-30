7688017c9e1969026e12a68e501a4024d1f04a3e
tomaszbabinski <tommybab@gmail.com>
2020-05-30 17:29:01 +0200

    Registering auth reducer in Store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { StoreModule } from '@ngrx/store';
 import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
+import { authReducer } from './auth/store/auth.reducer';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer
     AppRoutingModule,
     SharedModule,
     CoreModule,
-    StoreModule.forRoot({ shoppingList: shoppingListReducer })
+    StoreModule.forRoot({ shoppingList: shoppingListReducer, auth: authReducer })
   ],
   bootstrap: [AppComponent],
 
