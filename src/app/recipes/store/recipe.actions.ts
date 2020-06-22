ec55a5dd89ed2cdae2ca8cc2dd4a091ccdd17eef
tomaszbabinski <tommybab@gmail.com>
2020-06-22 20:23:46 +0200

    Adding fetch action to recipe actions

--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -2,10 +2,14 @@ import { Action } from '@ngrx/store';
 import { Recipe } from '../recipe.model';
 
 export const SET_RECIPES = '[Recipes] SET_RECIPES';
+export const FETCH_RECIPES = '[Recipes] FETCH_RECIPES';
 
 export class SetRecipes implements Action {
   readonly type = SET_RECIPES;
-
   constructor(public payload: Recipe[]) { }
 }
-export type RecipesActions = SetRecipes;
+
+export class FetchRecipes implements Action {
+  readonly type = FETCH_RECIPES;
+}
+export type RecipesActions = SetRecipes | FetchRecipes;
