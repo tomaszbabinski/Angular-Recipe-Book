c1e5d7395b85b091ba8a8a6699bf1436170d4ba0
tomaszbabinski <tommybab@gmail.com>
2020-06-25 21:43:49 +0200

    Adding reducer for add/update/delete recipe actions

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -18,6 +18,31 @@ export function recipeReducer(state = initialState, action: RecipesActions.Recip
         ...state,
         recipes: [...action.payload]
       };
+
+    case RecipesActions.ADD_RECIPE:
+      return {
+        ...state,
+        recipes: [...state.recipes, action.payload]
+      };
+    case RecipesActions.UPDATE_RECIPE:
+      const updatedRecipe = {
+        ...state.recipes[action.payload.index],
+        ...action.payload.newRecipe
+      };
+      const updatedRecipes = [...state.recipes];
+      updatedRecipes[action.payload.index] = updatedRecipe;
+      return {
+        ...state,
+        recipes: updatedRecipes
+      };
+    case RecipesActions.DELETE_RECIPE:
+
+      return {
+        ...state,
+        recipes: state.recipes.filter((recipe, index) => {
+          return index !== action.payload;
+        })
+      };
     default:
       return state;
   }
